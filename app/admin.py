472ad11c2cadee247434bdeb184e767678a3f1c4
Kbeach321 <iamkevinbeach@gmail.com>
2018-06-14 14:53:04 -0400

    working auth

---
+++ app/admin.py
@@ -0,0 +1,6 @@
+######## Imports ###########
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from app.models import User
+
+admin.site.register(User, UserAdmin)
---
+++ app/models.py
@@ -0,0 +1,8 @@
+########## Imports #######################
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+################# Classes #################
+
+class User(AbstractUser):
+    pass
