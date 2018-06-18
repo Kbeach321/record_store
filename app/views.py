066f411a3bd1875ca4a40d5d4ffad95509bc6168
Kbeach321 <iamkevinbeach@gmail.com>
2018-06-18 08:33:31 -0400

    defined views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from django.db.models import Q
+from app.permissions import IsOwnerOrReadOnly
+
+from app.serializers import BandSerializer, AlbumSerializer
+from app.models import Band, Album
+
+class BandListCreateAPIView(APIView):
+    def get:
+        pass
+    def post:
+        pass
+
+class BandRetrieveUpdateDestroyAPIView(APIView):
+    def get:
+        pass
+    def put:
+        pass
+    def delete:
+        pass
+
+class AlbumListCreateAPIView(APIView):
+    def get:
+        pass
+    def post:
+        pass
+
+class AlbumRetrieveUpdateDestroyAPIView(APIView):
+    def get:
+        pass
+    def put:
+        pass
+    def delete:
+        pass
